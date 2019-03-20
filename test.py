55c52d892383e7dd5b02d463b202a84041199dd3
Nicolas Roger <nicolas.roger@epita.fr>
2019-03-20 16:47:09 +0100

    lol

--- test.py
+++ test.py
@@ -12,16 +12,23 @@ def draw(G):
 
 def test_graph():
     G = nx.dodecahedral_graph()
-    shells = [[2, 3, 4, 5, 6], [8, 1, 0, 19, 18, 17, 16, 15, 14, 7], [9, 10, 11, 12, 13]]
-    nx.draw_shell(G, nlist=shells)
-    plt.show()
+    return G
+    #shells = [[2, 3, 4, 5, 6], [8, 1, 0, 19, 18, 17, 16, 15, 14, 7], [9, 10, 11, 12, 13]]
+    #nx.draw_shell(G, nlist=shells)
+    #plt.show()
 
 
 
-G = read_graph("inet")
+# grephi
+# Calculer la composante connexe géante
+#G = read_graph("inet")
+#G = test_graph()
+
+# G = nx.random_lobster(100, 0.9, 0.9)
+# G = nx.barabasi_albert_graph(100, 5)
+# G = nx.watts_strogatz_graph(30, 3, 0.1)
+# G = nx.erdos_renyi_graph(100, 0.15)
+
+print(G.number_of_nodes(), G.number_of_edges())
 draw(G)
-#gephi
-#script_dir = os.path.dirname(__file__)
-#print(script_dir)
-#Fh = open(os.path.join(script_dir, '~/Downloads/lol.txt'), 'r')
-#test_graph()
+
