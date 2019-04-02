681191b820606dd8ccb0377b19273e29504b02dd
Nicolas Roger <nicolas.roger@epita.fr>
2019-04-02 12:49:40 +0200

    lol

--- python-louvain/test.py
+++ python-louvain/test.py
@@ -2,21 +2,29 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import community
 import time
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib import cm
+import numpy as np
 
 def read_graph(path):
   mygraph = nx.read_edgelist(path)
   return mygraph
 
-def draw(G):
-    nx.draw(G, with_labels=True, font_weight='bold')
+def draw(Gs):
+    viridis = cm.get_cmap('viridis')
+    color = iter(viridis(np.linspace(0, 1, len(Gs))))
+    for G in Gs:
+        nx.draw(G, with_labels=True, font_weight='bold',
+                node_color=next(color))
     plt.show()
 
-def test_graph():
-    G = nx.dodecahedral_graph()
-    return G
-    #shells = [[2, 3, 4, 5, 6], [8, 1, 0, 19, 18, 17, 16, 15, 14, 7], [9, 10, 11, 12, 13]]
-    #nx.draw_shell(G, nlist=shells)
-    #plt.show()
+def best_partition(G):
+    start = time.time()
+    partition = community.best_partition(graph=G, randomize=False)
+    end = time.time()
+    print("partitioning took {}s".format(end - start))
+    print(("nodes : {}, edges : {}, partition : {}").format(G.number_of_nodes(), G.number_of_edges(), len(partition)))
+
 
 
 # https://networkx.github.io/documentation/latest/tutorial.html#multigraphs
@@ -26,25 +34,34 @@ def test_graph():
 # https://en.wikipedia.org/wiki/Louvain_Modularity
 # grephi
 # Calculer la composante connexe géante
+# https://hal.archives-ouvertes.fr/hal-01171295/document
 
 # G = nx.random_lobster(10, 0.9, 0.9)
 # G = nx.barabasi_albert_graph(100, 5)
 # G = nx.watts_strogatz_graph(5, 1, 0.1)
-G = nx.erdos_renyi_graph(10, 0.15)
-Go = nx.OrderedGraph(G) # not necessary in python > 3.6
+# G = nx.dodecahedral_graph()
 # G = test_graph()
 
 
+G = nx.erdos_renyi_graph(1000, 0.30)
+G = max(nx.connected_component_subgraphs(G), key=len)
+G = nx.OrderedGraph(G) # not necessary in python > 3.6
 
 #G = read_graph("inet")
 #print("G is a ",type(G), G.__dict__)
 print("loaded")
-partition1 = community.best_partition(graph=G, randomize=False)
-start = time.time()
-partition2 = community.best_partition(graph=G, randomize=False)
-end = time.time()
-print(end - start)
-print(("nodes : {}, edges : {}, partition1 : {}, partition2 : {} equals : {}").format(G.number_of_nodes(), G.number_of_edges(), len(partition1), len(partition2), partition1==partition2))
-#print(partition1, partition2)
-#draw(G)
+
+best_partition(G)
+
+
+bfs_order = list(nx.bfs_tree(G,0))
+#print(bfs_order)
+
+#print(G.node)
+
+mapping = dict(zip(G.node, bfs_order))
+#print(mapping)
+H = nx.relabel.relabel_nodes(G, mapping)
+best_partition(H)
+#draw([G, H])
 
